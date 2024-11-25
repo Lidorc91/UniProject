bcc79d4d0cd115b3c4831e0ba22eaa6bbda20020
mrlifelesss <benyehudaron@gmail.com>
2024-11-25 19:47:24 +0200

    Made UI BIGGER

--- UniProjectUI2/MainWindow.xaml.cs
+++ UniProjectUI2/MainWindow.xaml.cs
@@ -159,6 +159,12 @@ namespace UniProjectUI2
             DashGraph.Plot.Axes.Left.Label.FontSize = 50;
             DashGraph.Plot.Axes.Bottom.TickLabelStyle.FontSize = 40;
             DashGraph.Plot.Axes.Left.TickLabelStyle.FontSize = 40;
+
+            DevGraph.Plot.Axes.Title.Label.FontSize = 50;
+            DevGraph.Plot.Axes.Bottom.Label.FontSize = 50;
+            DevGraph.Plot.Axes.Left.Label.FontSize = 50;
+            DevGraph.Plot.Axes.Bottom.TickLabelStyle.FontSize = 40;
+            DevGraph.Plot.Axes.Left.TickLabelStyle.FontSize = 40;
             DevGraph.Refresh();
 
 
